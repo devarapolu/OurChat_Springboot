2cabde83cb69e2d0a697ae3333d97e974214e98b
11637721 <57550940+devarapolu@users.noreply.github.com>
2024-07-03 20:56:09 -0400

    Changed from WSS to WS

--- src/main/java/com/deva/ourchat/config/WebSocketConfig.java
+++ src/main/java/com/deva/ourchat/config/WebSocketConfig.java
@@ -9,9 +9,10 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/wss").withSockJS();
+        registry.addEndpoint("/ws").withSockJS();
     }
 
     @Override
